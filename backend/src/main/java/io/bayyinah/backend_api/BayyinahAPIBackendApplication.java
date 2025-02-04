8794196bec246009e5ce3fa27d999d98ea6057e6
Radwan Abu-Odeh <radwanizzat@gmail.com>
2025-02-04 10:29:24 -0500

    Spring Boot Backend application scaffolding
    Signed-off-by: Radwan Abu-Odeh <radwanizzat@gmail.com>

---
+++ backend/src/main/java/io/bayyinah/backend_api/BayyinahAPIBackendApplication.java
@@ -0,0 +1,13 @@
+package io.bayyinah.backend_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BayyinahAPIBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BayyinahAPIBackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/io/bayyinah/backend_api/BayyinahAPIBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.bayyinah.backend_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BayyinahAPIBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
